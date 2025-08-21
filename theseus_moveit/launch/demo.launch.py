c0f32488f2b060b2ab9860cff3cf3e15b638990c
laser <utbots.home@gmail.com>
2025-08-20 21:39:46 -0300

    model name corrected, but moveit doesnt connect to controllers

--- theseus_moveit/launch/demo.launch.py
+++ theseus_moveit/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("utbots_manipulator", package_name="theseus_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("theseus", package_name="theseus_moveit").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- theseus_moveit/launch/setup_assistant.launch.py
+++ theseus_moveit/launch/setup_assistant.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("utbots_manipulator", package_name="theseus_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("theseus", package_name="theseus_moveit").to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
