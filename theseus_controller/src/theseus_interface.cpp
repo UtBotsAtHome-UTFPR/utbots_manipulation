1e46b1c537612553c051b5fda8aa34c5b8063a25
gustavo-fardo <gustavo.fardo@hotmail.com>
2025-09-30 11:24:07 -0300

    gripper controller adjustments

--- theseus_controller/src/theseus_interface.cpp
+++ theseus_controller/src/theseus_interface.cpp
@@ -94,9 +94,9 @@ std::vector<hardware_interface::CommandInterface> TheseusInterface::export_comma
 CallbackReturn TheseusInterface::on_activate(const rclcpp_lifecycle::State & previous_state)
 {
     RCLCPP_INFO(rclcpp::get_logger("TheseusInterface"), "Starting manipulator hardware...");
-    position_commands_ = {0.0, 0.786, 4.71};
-    prev_position_commands_ = {0.0, 0.786, 4.71};
-    position_states_ = {0.0, 0.786, 4.71};
+    position_commands_ = {0.0, 0.786, 4.71, 0.0};
+    prev_position_commands_ = {0.0, 0.786, 4.71, 0.0};
+    position_states_ = {0.0, 0.786, 4.71, 0.0};
 
     try
     {
@@ -166,7 +166,11 @@ hardware_interface::return_type TheseusInterface::write(const rclcpp::Time & tim
     msg.append(compensateZeros(elbow));
     msg.append(std::to_string(elbow));
     msg.append(",");
-    //Add gripper
+    int gripper = static_cast<int>((position_commands_.at(2) * 180) / M_PI); 
+    msg.append("g");
+    msg.append(compensateZeros(gripper));
+    msg.append(std::to_string(gripper));
+    msg.append(",");
 
     try
     {
